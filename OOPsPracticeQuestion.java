5a1020aa391c5556a383758d60e0d62fef4687dd
aliarmaghan <114820182+aliarmaghan@users.noreply.github.com>
2023-05-25 23:06:50 +0530

    Oops Question

---
+++ OOPsPracticeQuestion.java
@@ -0,0 +1,12 @@
+class OOPsPracticeQuestion{
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "Ali Alphaits";
+        System.out.println(s.name);
+
+    }
+}
+class Student{
+    String name;
+    int marks;
+}
